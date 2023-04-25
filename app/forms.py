1fbb44b430086f9c112ad40fbd85fd1869c73d4f
CoolShade27 <andrei.calin321@yahoo.com>
2023-04-25 13:47:21 +0300

    Fully implemented followers

--- app/forms.py
+++ app/forms.py
@@ -41,3 +41,6 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Username already taken!')
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
